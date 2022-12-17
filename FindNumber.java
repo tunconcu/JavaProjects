e9c3ba76f3eb1279bd7dd1f5127b37641fe26170
Tunc Oncu <tunconcu2021@gmail.com>
2022-12-17 13:06:13 +0300

    first commit

---
+++ FindNumber.java
@@ -0,0 +1,22 @@
+package JavaProjects;
+import java.util.Scanner;
+public class FindNumber {
+    public static void main(String[] args) {
+
+        int[] nums = {1,2,3,4,5,6,7,8,9};
+		int target = 5;
+		boolean numExist = false;
+		
+		for(int num : nums) {
+			if(num == target) {
+				numExist = true;
+				break;
+			}else {
+				numExist = false;
+			}
+		}
+		System.out.println("Does number exist? " + numExist );
+        
+    }
+    
+}
