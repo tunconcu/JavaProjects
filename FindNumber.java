a203fa1b4022a6d3654ee97104e7756dec3f1996
Tunc Oncu <tunconcu2021@gmail.com>
2022-12-17 13:08:42 +0300

    add description

--- FindNumber.java
+++ FindNumber.java
@@ -2,7 +2,7 @@ package JavaProjects;
 import java.util.Scanner;
 public class FindNumber {
     public static void main(String[] args) {
-
+        System.out.println("Program to search for a specific number in an array");
         int[] nums = {1,2,3,4,5,6,7,8,9};
 		int target = 5;
 		boolean numExist = false;
